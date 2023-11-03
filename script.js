d5ec891adf49bcfc24e947c5323f7dc2d4b8b478
Sokol <sokolovskyy10@gmail.com>
2023-11-03 15:30:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict"
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "18")
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt ('What is your name?', '');
+// answers[1] = prompt ('What is your surname?', '');
+// answers[2] = prompt ('How old are you?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+// ІНТЕРПОЛЯЦІЯ
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan"
+
+// alert(`Hello, ${user}`)
+
+
+
+
+// ОПЕРАТОРИ
+
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+//     // ++incr;
+//     // --decr
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2+2*2 !== '6');
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || !isClose); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
